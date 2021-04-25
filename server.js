6304545d153d204303f174f460d2d405676cdaa5
duganla <59324021+duganla-dev@users.noreply.github.com>
2021-04-25 11:15:29 -0400

    Update server.js

--- server.js
+++ server.js
@@ -10,16 +10,47 @@ const wss = new WebSocket.Server({ port: 8080 });
 wss.on('connection', function connection(ws) {
     ws.on('message', function incoming(message) {
         message = JSON.parse(message);
-        console.log('Received: %s', message);
+        console.log('Received: %s : %s', ws.connectionRole, message);
 
         if (message.type === "establish") {
+            ws.connectionRole = "server";
+            ws.clientInfo = {name: message.name, id: message.id};
             console.log("New Server Connection:", message.name, "\n")
         };
 
         if (message.type === "clientEstablish") {
+            ws.connectionRole = "client"
             console.log("New Client Connection:")
         };
 
+
+        if (message.type === "getServerList") {
+            var serverList = []
+            wss.clients.forEach(function each(client) {
+                if (client.readyState === WebSocket.OPEN && client.connectionRole == 'server') {
+                    console.log(client.clientInfo)
+                    serverList.push(client.clientInfo)
+                }
+            });
+            ws.send(JSON.stringify({type: 'serverList', servers: serverList}))
+        }
+
+        if (message.type == 'getServerStats' && message.servers) {
+            wss.clients.forEach(function each(client) {
+                if (client.readyState === WebSocket.OPEN && client.connectionRole == 'server') {
+                    console.log(client.clientInfo)
+                    client.send(JSON.stringify({type:"performance", client: true}))
+                }
+            });
+        }
+
+        if (message.type === "performance") {
+            wss.clients.forEach(function each(client) {
+                if (client.readyState === WebSocket.OPEN && client.connectionRole == 'client') {
+                    client.send(JSON.stringify({type:"performance", server: ws.clientInfo, payload: message.payload}))
+                }
+            });
+        }
 
     });
 });
